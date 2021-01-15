fda03a15e822943f65258abfb33b6bd14d98ce4e
MackenzieAmoako23 <831590@mystma.org>
2021-01-15 16:30:29 +0000

    2- test -1

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f <= 32:
+        return "solid"
+    elif f >= 212:
+        return "gas"
+    else:
+        return "liquid"
+
+def isDozen(d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+
+def toGerman(word):
+    if word == "yes":
+        return "ja"
+    elif word == "no":
+        return "nein"
+
+def stopLight(c):
+    if c == "red":
+        return "stop"
+    elif c == "green":
+        return "go"
+    else:
+        return "slow"
+
